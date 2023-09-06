18b6735267de14a8eed89afbcca9d2ec3a60ef0d
Xrolllex <karolkaczanowski@wp.pl>
2023-09-06 20:31:12 +0200

    commit

---
+++ js/task-01.js
@@ -0,0 +1,20 @@
+const categoriesList = document.querySelector('#categories');
+
+const categoryItems = categoriesList.querySelectorAll('li.item');
+
+
+console.log(`Number of categories: ${categoryItems.length}`);
+
+
+categoryItems.forEach((categoryItem) => {
+  
+  const categoryName = categoryItem.querySelector('h2').textContent;
+
+  
+  const categoryElements = categoryItem.querySelectorAll('ul li');
+
+  
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${categoryElements.length}`);
+});
+
